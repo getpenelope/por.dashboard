0006c4e131be531a53922e5b1d6afc63b50ae677
Andrew Mleczko <andrew@mleczko.net>
2013-06-26 17:16:52 +0200

    Back to development: 1.7.14

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if sys.version_info[:3] < (2,5,0):
     requires.append('pysqlite')
 
 setup(name='por.dashboard',
-      version='1.7.13',
+      version='1.7.14.dev0',
       description='Penelope main package',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
